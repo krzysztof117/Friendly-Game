4bf83b67c5ae4a8c0251aff6006d3ad53bbf932c
Adri-Z <54725497+Adri-Z@users.noreply.github.com>
2019-09-15 11:16:14 -0500

    Primer ejemplo con Christopher
    Prueba para ver el acomodo de carpetas, archivos y contenidos.

---
+++ Starting/Example/ejemplo1.py
@@ -0,0 +1,9 @@
+#print("Mi primera clase con Krizf")
+#print("Estaba bueno el frapé")
+#print(" Y a   vamonos, ya me cansé !!!  Plisss !!!!! ")
+
+#Clase de PROGRAMACIÓN con ayudante Leonardo // Jueves 15 agosto 2019
+# ERRORES : Sintáctico , Lógico y Procesos no válidos.
+x=9
+print(x)
+#print(x=9)
